48d218a54883ff8db121565cecec5f0bfe600519
Andrew D. Bond <git.bond@xoxy.net>
2013-07-10 02:34:05 -0400

    Updating grader.js and checks.json for part 3

--- grader.js
+++ grader.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 /* jslint node: true */
-/*
+/**
+ * @author Startup Engineering course staff
+ * @author Andrew D. Bond <andrew.d.bond@gmail.com>
+ */
+ /*
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
 and basic DOM parsing.
@@ -29,16 +33,17 @@ var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
+    var util = require('util');
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
+        util.log(util.format("%s does not exist. Exiting.", instr));
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var cheerioHtml = function(html) {
+    return cheerio.load(html);
 };
 
 var loadChecks = function(checksfile) {
@@ -46,14 +51,37 @@ var loadChecks = function(checksfile) {
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+    checkHtml(cheerioHtml(fs.readFileSync(htmlfile)), checksfile);
+
+    return;
+};
+
+function checkHtml(jQuery, checksfile) {
+  $ = jQuery;
+  var checks = loadChecks(checksfile).sort();
+  var out = {};
+  for(var ii in checks) {
+      var present = $(checks[ii]).length > 0;
+      out[checks[ii]] = present;
+  }
+
+  var outJson = JSON.stringify(out, null, 4);
+  console.log(outJson);
+}
+
+var checkUrl = function(url, checksfile) {
+  var rest = require('restler');
+  var util = require('util');
+
+  rest.get(url).on('complete', function(result) {
+    if (result instanceof Error) {
+      util.error('Error: ' + result.message);
+    } else {
+      checkHtml(cheerioHtml(result), checksfile);
     }
-    return out;
+  });
+
+  return;
 };
 
 var clone = function(fn) {
@@ -64,12 +92,19 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to json checks file', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to HTML file', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var checkJson;
+    if (program.url)
+    {
+      checkUrl(program.url, program.checks);
+    }
+    else
+    {
+      checkHtmlFile(program.file, program.checks);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
